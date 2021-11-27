9226ecb266e4d8b368868cb59e2ec12047a6a267
songHyeongwon <iks1681@naver.com>
2021-11-27 23:30:57 +0900

    페이징 처리하기위해 기본 대상자 입력 해놓기

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,6 +11,28 @@ CREATE TABLE member (
 
 INSERT INTO member (name, password, auth, contract_number, create_datetime, update_datetime) VALUES
 ('admin', '{noop}secret', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test0', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test1', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test2', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test3', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test4', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test5', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test6', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test7', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test8', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+('test9', '{noop}test', 'ROLE_ADMIN', '000-0000-0000', now(), now()),
+
+('test10', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+('test11', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+('test12', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+('test13', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+('test14', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+('test15', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+('test16', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+('test17', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+('test18', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+('test19', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
+
 ('user1', '{noop}password1', 'ROLE_MEMBER', '000-0000-0000', now(), now()),
 ('user2', '{noop}password2', 'ROLE_MEMBER', '000-0000-0000', now(), now());
 
