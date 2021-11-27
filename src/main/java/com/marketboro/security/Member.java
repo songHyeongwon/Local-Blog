c2c53b3ac9fb4cd51a9ef6bc91f1f29c30ef7b37
songHyeongwon <iks1681@naver.com>
2021-11-27 22:07:06 +0900

    디버깅용 toString 추가

--- src/main/java/com/marketboro/security/Member.java
+++ src/main/java/com/marketboro/security/Member.java
@@ -9,16 +9,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.Setter;
+
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-@Entity @Getter
+@Entity
+@Getter
+@Setter
+@ToString
 @Table(name = "member")
 @AllArgsConstructor
 @NoArgsConstructor
