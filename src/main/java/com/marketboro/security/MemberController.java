e93fb70a89afbb99bbe0bbafd56ba4bdf4357415
songHyeongwon <iks1681@naver.com>
2021-11-27 22:07:40 +0900

    유저 리스트와 유저 조회 추가

--- src/main/java/com/marketboro/security/MemberController.java
+++ src/main/java/com/marketboro/security/MemberController.java
@@ -1,11 +1,15 @@
 package com.marketboro.security;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,8 +23,20 @@ public class MemberController {
 	}
 
 	@GetMapping("/user/list")
-	public String users(Map<String, Object> model) {
-		List<Member> members = memberRepository.findAll();
+	public String users(Map<String, Object> model, @RequestParam Map<String, String> param) {
+		System.out.println("name = " + param.get("name"));
+		String findName = (String)param.get("name");
+		List<Member> members = null;
+		if(findName == null) {
+			members = memberRepository.findAll();
+		} else {
+			members = memberRepository.findByNameLike(findName);
+		}
+		//디버깅용
+		/*for (int i = 0; i < members.size(); i++) {
+			System.out.println(members.get(i).toString());
+		}*/
+
 		model.put("members", members);
 		return "user";
 	}
--- src/main/java/com/marketboro/security/MemberRepository.java
+++ src/main/java/com/marketboro/security/MemberRepository.java
@@ -1,9 +1,11 @@
 package com.marketboro.security;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByName(String userId);
+	List<Member> findByNameLike(String userName);
 }
