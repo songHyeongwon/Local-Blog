fc0fcb13fb012d65e69aebafbdbc364c02b978cd
songHyeongwon <iks1681@naver.com>
2021-11-28 20:17:32 +0900

    비밀번호 검증 테스트

--- src/test/java/com/marketboro/security/SecurityApplicationTests.java
+++ src/test/java/com/marketboro/security/SecurityApplicationTests.java
@@ -3,11 +3,17 @@ package com.marketboro.security;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.regex.Pattern;
+
 @SpringBootTest
 class SecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("isNumeric");
+		System.out.println(isNumeric("daw123A"));
 	}
-
+	public boolean isNumeric(String str) {
+		return Pattern.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$", str);
+	}
 }
