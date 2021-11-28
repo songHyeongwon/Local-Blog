2a72c15f2a3eedf37650558095dd6b9eba0e2522
songHyeongwon <iks1681@naver.com>
2021-11-29 00:14:33 +0900

    멤버추가 테스트

--- src/test/java/com/marketboro/security/SecurityApplicationTests.java
+++ src/test/java/com/marketboro/security/SecurityApplicationTests.java
@@ -1,17 +1,37 @@
 package com.marketboro.security;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.regex.Pattern;
 
 @SpringBootTest
 class SecurityApplicationTests {
+	@Autowired
+	private MemberRepository memberRepository;
 
 	@Test
 	void contextLoads() {
-		System.out.println("isNumeric");
-		System.out.println(isNumeric("daw123A"));
+		for(int i = 0; i < 100; i++) {
+			Member member = new Member();
+			member.setId(				memberRepository.count()+1);
+			member.setName(				"song" + i);
+			member.setPassword(			("{noop}")+"test");
+			member.setContractNumber(	"010-9999-9999");
+			if(i < 50) {
+				member.setAuth(				"ROLE_ADMIN"); //사용자
+			} else {
+				member.setAuth(				"ROLE_MEMBER"); //사용자
+			}
+			member.setCreateDatetime(LocalDateTime.now());
+			member.setUpdateDatetime(LocalDateTime.now());
+			System.out.println(member.toString());
+			memberRepository.save(member);
+		}
+
+
 	}
 	public boolean isNumeric(String str) {
 		return Pattern.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$", str);
