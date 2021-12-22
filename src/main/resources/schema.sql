fe30e75e3cf04db6cf7cd2d60df82d480e0f53ce
songHyeongwon <iks1681@naver.com>
2021-12-22 19:16:24 +0900

    블로그 DB의 스키마 재정의
    블로그를 일단 스프링중심으로 만들고 NOde판도 만들어 보자

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,46 +6,36 @@ CREATE TABLE member (--글을 작성할수 있는 회원의 테이블
                        contract_number VARCHAR(50) NOT NULL,
                        create_datetime TIMESTAMP NOT NULL,
                        update_datetime TIMESTAMP NOT NULL,
-                       fail_cnt BIGINT DEFAULT 0 NULL,
-                       last_fail_time TIMESTAMP NULL,
+                       --fail_cnt BIGINT DEFAULT 0 NULL,
+                       --last_fail_time TIMESTAMP NULL,
                        PRIMARY KEY (id)
 );
 
 CREATE TABLE board (--게시글 내용
-                       id BIGINT AUTO_INCREMENT,
-                       name VARCHAR(50) NOT NULL,
-                       password VARCHAR(50) NOT NULL,
-                       auth VARCHAR(50) NOT NULL,
-                       contract_number VARCHAR(50) NOT NULL,
-                       create_datetime TIMESTAMP NOT NULL,
-                       update_datetime TIMESTAMP NOT NULL,
-                       fail_cnt BIGINT DEFAULT 0 NULL,
-                       last_fail_time TIMESTAMP NULL,
-                       PRIMARY KEY (id)
+                       boardId BIGINT AUTO_INCREMENT, -- 게시글 ID
+                       title VARCHAR(500) NOT NULL, -- 글제목
+                       create_datetime TIMESTAMP NOT NULL, -- 생성일
+                       update_datetime TIMESTAMP NOT NULL, -- 수정일
+                       PRIMARY KEY (boardId)
 );
 
 CREATE TABLE board_desc (--게시글 상세
-                       id BIGINT AUTO_INCREMENT,
-                       name VARCHAR(50) NOT NULL,
-                       password VARCHAR(50) NOT NULL,
-                       auth VARCHAR(50) NOT NULL,
-                       contract_number VARCHAR(50) NOT NULL,
-                       create_datetime TIMESTAMP NOT NULL,
-                       update_datetime TIMESTAMP NOT NULL,
-                       fail_cnt BIGINT DEFAULT 0 NULL,
-                       last_fail_time TIMESTAMP NULL,
-                       PRIMARY KEY (id)
+                       board_descId BIGINT AUTO_INCREMENT, -- 게시글 상세 ID
+                       boardId BIGINT AUTO_INCREMENT, -- 게시글 ID
+                       text VARCHAR(2000) NOT NULL, --글 내용 차후 html 태그로 변경할것
+                       create_datetime TIMESTAMP NOT NULL, --생성일
+                       update_datetime TIMESTAMP NOT NULL, --수정일
+                       PRIMARY KEY (board_descId)
 );
 CREATE TABLE menu (--메뉴리스트
-                       id BIGINT AUTO_INCREMENT,
-                       name VARCHAR(50) NOT NULL,
-                       password VARCHAR(50) NOT NULL,
-                       auth VARCHAR(50) NOT NULL,
-                       contract_number VARCHAR(50) NOT NULL,
-                       create_datetime TIMESTAMP NOT NULL,
-                       update_datetime TIMESTAMP NOT NULL,
-                       fail_cnt BIGINT DEFAULT 0 NULL,
-                       last_fail_time TIMESTAMP NULL,
+                       id BIGINT AUTO_INCREMENT, --메뉴ID
+                       menuName VARCHAR(500) NOT NULL, -- 메뉴명
+                       menuUrl VARCHAR(500) NOT NULL, --메뉴의 URL
+                       menuParentId BIGINT NULL, --부모 메뉴 ID
+                       depth BIGINT NULL, -- 메뉴의 깊이 순서
+                       menuSort BIGINT, -- 메뉴의 정렬 기준
+                       create_datetime TIMESTAMP NOT NULL, -- 생성일
+                       update_datetime TIMESTAMP NOT NULL, -- 수정일
                        PRIMARY KEY (id)
 );
 
