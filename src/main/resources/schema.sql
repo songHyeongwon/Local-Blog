65c3e0483e6a44d5f2ece829c6bc97ba2cc80c29
songHyeongwon <iks1681@naver.com>
2021-12-26 01:14:49 +0900

    시퀀스 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,14 @@
-drop table if exists member;
-drop table if exists board;
-drop table if exists board_desc;
-drop table if exists menu;
+DROP TABLE IF EXISTS MEMBER;
+DROP TABLE IF EXISTS BOARD;
+DROP TABLE IF EXISTS BOARD_DESC;
+DROP TABLE IF EXISTS MENU;
 
+DROP SEQUENCE IF EXISTS MEMBER_SEQ;
+DROP SEQUENCE IF EXISTS BOARD_SEQ;
+DROP SEQUENCE IF EXISTS BOARD_DESC_SEQ;
+DROP SEQUENCE IF EXISTS MENU_SEQ;
 
-CREATE TABLE member (--글을 작성할수 있는 회원의 테이블
+CREATE TABLE MEMBER (--글을 작성할수 있는 회원의 테이블
                        id BIGINT AUTO_INCREMENT,
                        name VARCHAR(50) NOT NULL,
                        password VARCHAR(50) NOT NULL,
@@ -17,7 +21,7 @@ CREATE TABLE member (--글을 작성할수 있는 회원의 테이블
                        PRIMARY KEY (id)
 );
 
-CREATE TABLE board (--게시글 내용
+CREATE TABLE BOARD (--게시글 내용
                        boardId BIGINT AUTO_INCREMENT, -- 게시글 ID
                        title VARCHAR(500) NOT NULL, -- 글제목
                        create_datetime TIMESTAMP NOT NULL, -- 생성일
@@ -25,7 +29,7 @@ CREATE TABLE board (--게시글 내용
                        PRIMARY KEY (boardId)
 );
 
-CREATE TABLE board_desc (--게시글 상세
+CREATE TABLE BOARD_DESC (--게시글 상세
                        board_descId BIGINT AUTO_INCREMENT, -- 게시글 상세 ID
                        boardId BIGINT AUTO_INCREMENT, -- 게시글 ID
                        text VARCHAR(2000) NOT NULL, --글 내용 차후 html 태그로 변경할것
@@ -33,7 +37,7 @@ CREATE TABLE board_desc (--게시글 상세
                        update_datetime TIMESTAMP NOT NULL, --수정일
                        PRIMARY KEY (board_descId)
 );
-CREATE TABLE menu (--메뉴리스트
+CREATE TABLE MENU (--메뉴리스트
                        id BIGINT AUTO_INCREMENT, --메뉴ID
                        menuName VARCHAR(500) NOT NULL, -- 메뉴명
                        menuUrl VARCHAR(500) NOT NULL, --메뉴의 URL
@@ -44,3 +48,8 @@ CREATE TABLE menu (--메뉴리스트
                        update_datetime TIMESTAMP NOT NULL, -- 수정일
                        PRIMARY KEY (id)
 );
+
+CREATE SEQUENCE MEMBER_SEQ START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE BOARD_SEQ START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE BOARD_DESC_SEQ START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE MENU_SEQ START WITH 1 INCREMENT BY 1;
