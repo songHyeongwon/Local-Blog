dae6e4e4378af7fabe687e495dccd37e3d52f1ae
songHyeongwon <iks1681@naver.com>
2021-12-26 01:15:14 +0900

    메뉴 테스트용 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO member (name, password, auth, contract_number, create_datetime, update_datetime) VALUES
 ('admin', '{noop}test', 'ROLE_MEMBER', '000-0000-0000', now(), now());
+
+
+INSERT INTO MENU (menuName, menuUrl, menuParentId, depth, menuSort, create_datetime,update_datetime) VALUES
+('전체', 'aaa', null, null, now(), now());
