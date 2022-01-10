e3183b43b37d9e0992a80cf62493b0ac02f85e96
songHyeongwon <iks1681@naver.com>
2022-01-10 21:39:05 +0900

    인덱스 폼 수정

--- src/main/java/com/myblog/controller/MainController.java
+++ src/main/java/com/myblog/controller/MainController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 public class MainController {
     @GetMapping({"/", "index"})
     public String mainPage(Map<String, Object> model) {
-        System.out.println("들어오냐?");
+        System.out.println("mainPage");
         return "index";
     }
 
